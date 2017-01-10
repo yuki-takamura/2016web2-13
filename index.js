ff4f9f5e858665441c4489b80cf91f9ce77d20eb
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2017-01-10 16:02:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,52 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var Connection = require('tedious').Connection;
+  var config = {
+    userName: '',
+    password: '',
+    server: '',
+    options: {encrypt: true, database : ''}
+  };
+  var connection = new Connection(config);
+  connection.on('connect', function(err){
+    if(err){
+       res.render('index', { title : "はじめてのDB",message:err});
+    }else{
+        console.log("Connected");
+        executeStatement();
+    }
 });
 
+ var Request = require('tedious').Request;
+ var TYPES = require('tedious').TYPES;
+  
+ function executeStatement() {
+   request = new Request("SELECT TOP(10) CompanyName FROM SalesLT.Customer;", function(err){
+     if(err){
+       console.log(err);}
+   });
+   
+ var result = '<table>';
+   request.on('row', function(columns) {
+     result += '<tr>';
+     columns.forEach(function(column){
+       if(column.value === null){
+         console.log('NULL');
+       }else{
+         result += '<td>' + column.value + '</td>';
+       }
+       result += '</tr>';
+     });
+   });
+   
+   request.on('doneProc', function(rowCount, more, returnStatus, rows){
+     result += "</table>";
+     res.render('index', {title : "はじめてのDB",message:result});
+   });
+   
+   connection.execSql(request);
+ }
+});
+  
 module.exports = router;
